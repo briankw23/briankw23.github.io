23bbd559646384c73633939ff444efd10ca08153
Brian Wright <briankw23@gmail.com>
2018-06-25 13:02:52 -0500

    added event listeners for projects and blogs, styled buttons

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,7 +1,20 @@
-const projectStringBuilder = () => {
+const projectStringBuilder = (projectArray) => {
+  let strang = '';
+  strang += projectArray;
+  printToDom(strang,'#projects');
+};
 
+const blogStringBuilder = (blogArray) => {
+  let strang = '';
+  strang += blogArray;
+  printToDom(strang,'#blogs');
+};
+
+const printToDom = (string, whereToPrint) => {
+  $(whereToPrint).html(string);
 };
 
 module.exports = {
   projectStringBuilder,
+  blogStringBuilder,
 };
--- javascripts/events.js
+++ javascripts/events.js
@@ -1,11 +1,12 @@
 const firebaseApi = require('./firebase');
+const dom = require('./dom');
 
 const showProjects = (e) => {
   $(document).on('click', '.portfolio', (e) => {
     firebaseApi.getProjects()
       .then((results) => {
         console.log(results);
-
+        dom.projectStringBuilder(results);
       })
       .catch((error) => {
         console.error(error);
@@ -18,7 +19,7 @@ const showBlogs = (e) => {
     firebaseApi.getBlogs()
       .then((results) => {
         console.log(results);
-
+        dom.blogStringBuilder(results);
       })
       .catch((error) => {
         console.error(error);
@@ -26,9 +27,25 @@ const showBlogs = (e) => {
   });
 };
 
+const showContact = (e) => {
+  $(document).on('click', '.contact', (e) => {
+    let strang = '';
+    strang += 'contact info should print here';
+    $('#contact').html(strang);
+  });
+};
+
+const showRefresh = () => {
+  $(document).on('click', '.home', (e) => {
+    document.reload();
+  });
+};
+
 const initializer = () => {
   showProjects();
   showBlogs();
+  showContact();
+  showRefresh();
 };
 
 module.exports = {
